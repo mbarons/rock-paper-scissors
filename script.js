45f6b49fa886fd1ab6828cb31a1b00ca248ba165
marcobaron <marcobaron@hotmail.com>
2022-05-02 18:36:34 -0300

    Write function for one round and to ask input

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ function computerPlay(){
     }
     return computerChoice
 }
+
+function oneRound(playerSelection, computerSelection) {
+    let round_winner
+    if (playerSelection == computerSelection){ 
+        round_winner = "tie"
+    }
+    else if (playerSelection == "stone" && computerSelection == "paper"){
+        round_winner = "computer"
+    }
+    else if (playerSelection == "stone" && computerSelection == "scissors"){
+        round_winner = "player"
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors"){
+        round_winner = "computer"
+    }
+    else if (playerSelection == "paper" && computerSelection == "stone"){
+        round_winner = "player"
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper"){
+        round_winner = "player"
+    }
+    else if (playerSelection == "scissors" && computerSelection == "stone"){
+        round_winner = "computer"
+    }
+    return round_winner
+}
+
+function askInput() {
+    let userDecision = prompt("Please, write your decision: Rock, Paper or Scissors")
+    return userDecision.toLowerCase()
+}
+
+userInput = askInput()
+console.log(userInput)
